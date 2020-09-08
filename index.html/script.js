cc8fd42c0cef56888fc6fe294742a023273d5ee9
solomonmiu <69608424+solomonmiu@users.noreply.github.com>
2020-09-08 15:57:00 -0700

    updated

--- index.html/script.js
+++
@@ -1,6 +0,0 @@
-function showFullname(){
-    let fname = document.getElementById("first_name").value;
-    let lname = document.getElementById("last_name").value;
-let fullname = fname+" "+lname;
-document.getElementById("output").innerHTML = fullname;
-}
